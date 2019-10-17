571a32500bea992699bd51c70082e570a2447346
Michael Hospedales <hospedales@me.com>
2019-10-17 09:39:13 -0400

    Add files via upload

---
+++ precache-manifest.79918f65e926cda9bd5dfa8c45574e3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b545c985c2ff57677cda01902f398cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af21a243194b4c569b7",
+    "url": "/static/css/main.18dcdc52.chunk.css"
+  },
+  {
+    "revision": "fd554c1d7bd202c49640",
+    "url": "/static/js/2.e8fa88b7.chunk.js"
+  },
+  {
+    "revision": "5af21a243194b4c569b7",
+    "url": "/static/js/main.b71cdb23.chunk.js"
+  },
+  {
+    "revision": "fb0497c3b32940f266de",
+    "url": "/static/js/runtime-main.53088f09.js"
+  }
+]);
